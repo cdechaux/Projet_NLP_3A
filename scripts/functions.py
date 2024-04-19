44d3e07deba4d2ec1c4b9080de560a08240e4d98
cdechaux <claire.dechaux@ensae.fr>
2024-04-19 17:19:11 +0000

    commit

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,7 +1,19 @@
+import pandas as pd
+import json
 import yaml
 import re
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from wordcloud import WordCloud, ImageColorGenerator
 import unicodedata
+from dotenv import load_dotenv
+import os
+from huggingface_hub import notebook_login
+from collections import Counter
+from PIL import Image
 import string
+from datasets import Dataset, Sequence, ClassLabel
 
 
 def load_labels(file_path: str) -> dict:
@@ -107,6 +119,7 @@ def fill_tag_dictionary(data_lines, list_tags, pattern):
 
     return data_dict
 
+
 def check_empty(row):
     """Function to check if one column is filled and the other empty
 
@@ -114,14 +127,14 @@ def check_empty(row):
         row : row of dataframe
 
     Returns:
-        bool : False il both are empty or full 
+        bool : False il both are empty or full
     """
     if row["surname"] != "" and row["surname_household"] == "":
         return True
     elif row["surname"] == "" and row["surname_household"] != "":
         return True
     else:
-        return False  
+        return False
 
 
 def check_both_not_empty(row):
@@ -138,9 +151,10 @@ def check_both_not_empty(row):
     else:
         return False
 
+
 # Définir une fonction pour fusionner les colonnes
 def fusionner_colonnes(colonne1, colonne2):
-    """ Merge two columns for one row
+    """Merge two columns for one row
 
     Args:
         colonne1 (str): first column to merge
@@ -180,3 +194,91 @@ def nombre_voyelles(chaine):
 def nombre_consonnes(chaine):
     return sum(c.isalpha() and c.lower() not in "aeiou" for c in str(chaine))
 
+
+def create_text_tag_arrays(df):
+    """
+    Create arrays of texts and corresponding tags from a DataFrame.
+
+    Args:
+    df (DataFrame): DataFrame containing text data with corresponding tags.
+
+    Returns:
+    texts (list): List to store text data.
+    tags (list): List to store corresponding tags.
+    """
+    # Create empty arrays to store texts and tags
+    texts = []
+    tags = []
+
+    # Iterate over each row of the DataFrame
+    for _, row in df.iterrows():
+        # Initialize temporary lists to store texts and tags for the current row
+        row_texts = []
+        row_tags = []
+
+        # Iterate over each element of the row
+        for col, value in row.items():
+            # Ignore empty values
+            if pd.notna(value) and value != "":
+                # Add corresponding text and tag to temporary lists
+                row_texts.append(value)
+                row_tags.append(col)
+
+        # Add temporary lists to the main arrays
+        texts.append(row_texts)
+        tags.append(row_tags)
+
+    return texts, tags
+
+
+def separate_words_with_space(texts, tags):
+    """
+    Function to separate words containing a space followed by a character into two new words.
+
+    Args:
+    texts (list): List of text data.
+    tags (list): List of corresponding tags.
+
+    Returns:
+    new_texts (list): List of separated text data.
+    new_tags (list): List of corresponding separated tags.
+    """
+    new_texts = []
+    new_tags = []
+
+    for text_list, tag_list in zip(texts, tags):
+        separated_text_list = []
+        separated_tag_list = []
+
+        for word, tag in zip(text_list, tag_list):
+            # Check if the word contains a space followed by a character
+            if " " in word:
+                parts = word.split(" ")
+                for i, part in enumerate(parts):
+                    # Add the separated word
+                    separated_text_list.append(part)
+
+                    # Add the corresponding tag
+                    if i == 0:
+                        separated_tag_list.append("B-" + tag)
+                    else:
+                        separated_tag_list.append("I-" + tag)
+            else:
+                separated_text_list.append(word)
+                separated_tag_list.append("B-" + tag)
+
+        new_texts.append(separated_text_list)
+        new_tags.append(separated_tag_list)
+
+    return new_texts, new_tags
+
+
+def create_dataset(texts, tags, desired_label_order):
+    data = {
+        "id": [str(i) for i in range(len(texts))],
+        "tokens": texts,
+        "ner_tags": tags,
+    }
+    return Dataset.from_dict(data).cast_column(
+        "ner_tags", Sequence(ClassLabel(names=desired_label_order, num_classes=22))
+    )
